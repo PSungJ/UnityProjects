d3ab9930cb9eab6499ccb186a4cb154b7d5f403c
SeongJun Park <ovweyt@naver.com>
2025-05-14 10:17:26 +0900

    VS로 작성한 C# Code
    250514

---
+++ ExtensionMethod.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+///*확장 메서드 : 부모클래스를 물려받아 자식클래스를 만든 뒤 여기에 메소드나 필드를
+// 추가하는 것이 아닌 기존의 클래스에 메소드를 추가,확장하는 방법*/
+//namespace _05_12_Csharp
+//{
+//    public static class InterExtension
+//    {
+//        //확장 메서드 사용법
+//        public static int Square(this int myInt)
+//        {
+//            return myInt * myInt;
+//        }
+//        public static int Power(this int myInt, int exponent)
+//        {
+//            int result = myInt;
+//            for (int i = 1;  i < exponent; i++)
+//            {
+//                result *= myInt;
+//            }
+//            return result;
+//        }
+//    }
+    
+//    internal class ExtensionMethod
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine($"3^2 : {3.Square()}");
+//            Console.WriteLine($"3^4 : {3.Power(4)}");
+//            Console.WriteLine($"2^10 : {2.Power(10)}");
+//        }
+//    }
+//}
---
+++ Partial.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _05_12_Csharp
+//{
+//    //분할 클래스 : 클래스의 구현이 길어질 경우 여러파일로 나누어 구현할 수 있기 위해
+//    partial class MyClass
+//    {
+//        public void Method_A()
+//        {
+//            Console.WriteLine("Method_A");
+//        }
+//        public void Method_B()
+//        {
+//            Console.WriteLine("Method_B");
+//        }
+//    }
+//    partial class MyClass
+//    {
+//        public void Method_C()
+//        {
+//            Console.WriteLine("Method_C");
+//        }
+//        public void Method_D()
+//        {
+//            Console.WriteLine("Method_D");
+//        }
+//    }
+//    internal class Partial
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyClass myClass = new MyClass();
+//            myClass.Method_A();
+//            myClass.Method_B();
+//            myClass.Method_C();
+//            myClass.Method_D();
+//            Console.ReadLine();
+//        }
+//    }
+//}
